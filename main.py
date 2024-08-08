4f1115fd8dae61df250b5d8dcf7165e76f37bb59
Michael Byers <mikebyers24@gmail.com>
2024-08-08 15:58:28 -0400

    smtp practice using with keyword.

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import smtplib
 my_email = 'mike.test.2424@gmail.com'
 my_password = 'pxrt sqnz mqjq kaxc'
 
-connection = smtplib.SMTP('smtp.gmail.com', 587)
-connection.starttls()
-connection.login(my_email, my_password)
-connection.sendmail(from_addr=my_email, to_addrs='miketest2424@yahoo.com', msg='Subject:Hello\n\nTesting Testing 123...')
-connection.close()
+with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+    connection.starttls()
+    connection.login(my_email, my_password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs='miketest2424@yahoo.com',
+        msg='Subject:Hello Again\n\nTesting Testing 123...')
