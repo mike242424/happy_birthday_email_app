3bec24d60f24234f54528eae5e74209dc7d93f06
Michael Byers <mikebyers24@gmail.com>
2024-08-09 17:25:29 -0400

    refactored open file inside if statement.

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ my_password = 'pxrt sqnz mqjq kaxc'
 now = dt.datetime.now()
 
 data = pd.read_csv('birthdays.csv').to_dict('records')
-with open(f'./letter_templates/letter_{random.randint(1, 3)}.txt', 'r') as letter_templates:
-    letter = letter_templates.read()
 
 for item in data:
     if item['month'] == now.month and item['day'] == now.day:
+        with open(f'./letter_templates/letter_{random.randint(1, 3)}.txt', 'r') as letter_templates:
+            letter = letter_templates.read()
+
         new_letter = letter.replace('[NAME]', item['name'])
         with smtplib.SMTP('smtp.gmail.com', 587) as connection:
             connection.starttls()
@@ -21,7 +22,3 @@ for item in data:
                 from_addr=my_email,
                 to_addrs=item['email'],
                 msg=f'Subject:Happy Birthday\n\n{new_letter}')
-
-
-
-
