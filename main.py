b807146a07818f4b27cd49fc0ea953a6d113b0c3
Michael Byers <mikebyers24@gmail.com>
2024-08-09 16:58:07 -0400

    working happy birthday sending functionality.

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
+import random
 import smtplib
+import datetime as dt
+import pandas as pd
 
 my_email = 'mike.test.2424@gmail.com'
 my_password = 'pxrt sqnz mqjq kaxc'
+now = dt.datetime.now()
 
-with smtplib.SMTP('smtp.gmail.com', 587) as connection:
-    connection.starttls()
-    connection.login(my_email, my_password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs='miketest2424@yahoo.com',
-        msg='Subject:Hello Again\n\nTesting Testing 123...')
+data = pd.read_csv('birthdays.csv').to_dict('records')
+with open(f'./letter_templates/letter_{random.randint(1, 3)}.txt', 'r') as letter_templates:
+    letter = letter_templates.read()
+
+for item in data:
+    if item['year'] == now.year and item['month'] == now.month and item['day'] == now.day:
+        new_letter = letter.replace('[NAME]', item['name'])
+        with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+            connection.starttls()
+            connection.login(my_email, my_password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs=item['email'],
+                msg=f'Subject:Happy Birthday\n\n{new_letter}')
+
+
+
+
